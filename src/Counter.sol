106a03c6a2f56003df82546e11bf91079e49469a
Anuoluwapo Afolami <hesedanu@gmail.com>
2026-01-16 23:12:47 +0100

    wer

--- src/Counter.sol
+++ src/Counter.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.8.26;
  */
 contract SimpleVault {
     // Fallback function to receive ETH {
-        totalDeposits += msg.
         emit Deposit(msg.sender, msg.value, block.timestamp);
     }
 }
