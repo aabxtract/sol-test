a93baec3577888eb30239c51f38a77a5d44f3e9e
Anuoluwapo Afolami <hesedanu@gmail.com>
2026-01-20 22:07:34 +0100

    ffg

--- src/simplestorage.sol
+++ src/simplestorage.sol
@@ -1,20 +1,21 @@
 //SPDX LICENSE IDENTIFIER: MIT
 pragma solidity ^0.8.30;
 
-contract Storage{
-    struct Store{
+contract Storage {
+    struct Store {
         string name;
         uint age;
     }
 
     Store[] public store;
 
-    mapping (uint=>string) public name;
+    mapping(uint => string) public name;
 
-    function add(string memory _name, uint _age){
-        store.push(Store(_name,_age));
+    function add(string memory _name, uint _age) {
+        store.push(Store(_name, _age));
     }
 
-    function 
+    function get(uint _index) public view returns (string memory, uint) {
+        return (store[_index].name, store[_index].age);
+    }
 }
- 
