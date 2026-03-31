01ead95c006efca1b5c0005ee7d7eaaa3d8e9c3d
aabxtract <hesedanu@gmail.com>
2026-03-31 18:32:29 +0100

    hdhdh

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,4 +5,26 @@ pragma solidity ^0.8.26;
  * @dev A basic vault contract for Base L2 that allows users to deposit and withdraw ETH
  * @notice This demonstrates common patterns for Base L2 smart contracts
  */
-contract 
+contract SimpleCounter {
+    uint256 public count;
+    address public owner;
+    event CountIncreased(uint256 newCount, address indexed user);
+    event CountReset(uint256 newCount, address indexed user);
+    constructor() {
+        owner = msg.sender;
+    }
+    function increment() public {
+        count++;
+        emit CountIncreased(count, msg.sender);
+    }
+    function reset() public {
+        count = 0;
+        emit CountReset(count, msg.sender);
+    }
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+    function getOwner() public view returns (address) {
+        return own
+    }
+}
