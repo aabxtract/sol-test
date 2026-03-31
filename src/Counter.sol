bf800b8b0709abf789f3678ca46dbedd03a36dd0
aabxtract <hesedanu@gmail.com>
2026-03-31 19:03:15 +0100

    jjj

--- src/Counter.sol
+++ src/Counter.sol
@@ -24,4 +24,4 @@ contract SimpleCounter {
     function getCount() public view returns (uint256) {
         return count;
     }
-    function getOwner() public vie
+    function getOwner() public vi
