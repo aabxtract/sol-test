4d90cb29f1971f3b97fe2bcbceeb0fe3c2287abb
aabxtract <hesedanu@gmail.com>
2026-04-16 17:17:15 +0100

    game on

--- generate_commits.ps1
+++ generate_commits.ps1
@@ -2,7 +2,7 @@ $targetFile = "src\simplestorage.sol"
 
 for ($i = 1; $i -le 200; $i++) {
     # Generate a random string
-    $randomStr = -join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})
+    $randomStr = -join ((65..90) + (97..122) | Get-Random -Count 10 | % { [char]$_ })
     
     # Append a comment to the file
     Add-Content -Path $targetFile -Value "// Random commit comment $i - $randomStr"
